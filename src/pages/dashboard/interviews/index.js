e19de7c79bf142c9bef363b471284f119ac6ce0d
Universz666 <kitiphon.pa.61@ubu.ac.th>
2022-06-21 00:58:51 +0700

    lasted

--- src/pages/dashboard/interviews/index.js
+++ src/pages/dashboard/interviews/index.js
@@ -1,12 +1,14 @@
 import { React, useState } from "react";
 import { Flex, Box } from "reflexbox";
-import { Switch } from "antd";
+import { Button, Switch } from "antd";
+import { useRouter } from "next/router";
 
 import CompInterview from "../../../components/Dashboard/_interviews";
 import Landing from "../../../components/LayoutsLanding";
 
 function Interviews() {
   const [checkstate, setCheckStae] = useState(true);
+  const router = useRouter();
 
   function onChange(checked) {
     setCheckStae(checked);
@@ -25,6 +27,9 @@ function Interviews() {
             defaultChecked
             onChange={onChange}
           />
+        </Box>
+        <Box>
+          <Button className="btn-login" style={{width:"80px", height:"30px"}} onClick={() => router.push("/")}>Home</Button>
         </Box>
       </Flex>
       {checkstate ? <CompInterview /> : <></>}
